3905aab612fe90c02b9ff841b8d94f1b0f34cc24
h493 <chhikarahimanshu21@gmail.com>
2026-09-01 14:14:02 +0530

    bug fix matching

--- order-service/src/main/java/com/microservice/ecommerce/order_service/config/AppConfig.java
+++ order-service/src/main/java/com/microservice/ecommerce/order_service/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.microservice.ecommerce.order_service.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,10 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        // STANDARD matching is fuzzy enough to map OrderRequestItemDto.productId
+        // onto OrderItem.id, which makes Hibernate treat new items as detached.
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 }
