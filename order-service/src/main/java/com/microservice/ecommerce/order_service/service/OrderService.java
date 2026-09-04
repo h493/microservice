e65e350f556f5d6f170d89a3bf4296bfd00eeaab
h493 <chhikarahimanshu21@gmail.com>
2026-09-04 11:25:09 +0530

    circuit breakers

--- order-service/src/main/java/com/microservice/ecommerce/order_service/service/OrderService.java
+++ order-service/src/main/java/com/microservice/ecommerce/order_service/service/OrderService.java
@@ -6,6 +6,7 @@ import com.microservice.ecommerce.order_service.entity.OrderItem;
 import com.microservice.ecommerce.order_service.entity.OrderStatus;
 import com.microservice.ecommerce.order_service.entity.Orders;
 import com.microservice.ecommerce.order_service.repository.OrderRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
@@ -40,8 +41,9 @@ public class OrderService {
                 .orElseThrow(() -> new RuntimeException("Order was not found"));
     }
 
-    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
-    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
         log.info("Calling the createOrder Method");
         Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
