43aa6e1142800bdcfe0aa268852cda796964b9df
h493 <chhikarahimanshu21@gmail.com>
2026-09-06 20:58:02 +0530

    zipkin

--- order-service/src/main/java/com/microservice/ecommerce/order_service/config/AppConfig.java
+++ order-service/src/main/java/com/microservice/ecommerce/order_service/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.microservice.ecommerce.order_service.config;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +18,10 @@ public class AppConfig {
         // onto OrderItem.id, which makes Hibernate treat new items as detached.
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 }
